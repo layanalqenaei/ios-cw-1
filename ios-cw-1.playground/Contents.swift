5f54fbf131b3917356382515c8723df62262496e
layanalqenaei <70814420+layanalqenaei@users.noreply.github.com>
2020-09-09 20:17:15 +0300

    donee

---
+++ ios-cw-1.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var str = "Hello, playground"
+let name = "Layan"
+var hobby1 = "photography"
+var hobby2 = "swiming"
+var age = 17
+var height = 1.72
+
+print( "My name is \(name) my hobbies: \(hobby1) \(hobby2) my age\(age) my height\(height)")
+
+let friendname = "ghadeer"
+var friendhobby1 = "painting"
+var friendhobby2 = "running"
+var friendage = 17
+var friendheight = 1.67
+
+print( "friends name is\(friendname) my friends hobbies: \(friendhobby1) \(friendhobby2) my friends age\(friendage) my friends height\(friendheight)")
+
+let agedifference = age - friendage
+
+print( agedifference)
+
+age == friendage
+height == friendheight
+hobby1 == friendhobby1
+hobby2 == friendhobby2
